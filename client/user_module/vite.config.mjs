d83dbbbab3ab1edfbf9d678bf14eba830241df11
Daniel Abrao <danielpaladar@gmail.com>
2024-07-27 13:45:44 -0300

    Update vite configs to accept dynamic port and host input

--- client/user_module/vite.config.mjs
+++ client/user_module/vite.config.mjs
@@ -7,6 +7,12 @@ export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
+  const VITE_PORT = process.env.VITE_PORT || 5173;
+  const API_APP_PROTOCOL =
+    process.env.APP_PROTOCOL || process.env.API_APP_PROTOCOL || "http";
+  const API_APP_HOST =
+    process.env.APP_HOST || process.env.API_APP_HOST || "0.0.0.0";
+  const API_APP_PORT = process.env.APP_PORT || process.env.API_APP_PORT || 4025;
   return {
     // vite config
     plugins: [vue()],
@@ -16,8 +22,9 @@ export default defineConfig(({ command, mode }) => {
     root: "./client/user_module",
     base: "/app",
     server: {
+      port: VITE_PORT,
       proxy: {
-        "/api": "http://localhost:4022"
+        "/api": `${API_APP_PROTOCOL}://${API_APP_HOST}:${API_APP_PORT}`
       }
     }
   };
