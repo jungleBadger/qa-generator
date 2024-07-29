cea0e46476f3345f1233038b0eea0f8729380a05
Alexia Balionis <alexia.balionis@olxbr.com>
2024-07-29 16:44:14 -0300

    Update user module fonts and styles

--- client/user_module/src/pages/Home.tsx
+++ client/user_module/src/pages/Home.tsx
@@ -3,9 +3,7 @@ function Home() {
     <div className="w-full flex flex-col justify-center items-center">
       <div className="flex justify-center w-full">
         <div className="w-2/3">
-          <h1 className="text-3xl font-bold">
-            Bem-vindo ao sistema de estudos
-          </h1>
+          <h1 className="text-3xl font-bold">Bem-vindo ao LearningFlash</h1>
           <p className="mt-4">
             Transforme textos em perguntas e respostas para facilitar seu
             aprendizado. Comece agora e melhore sua retenção de informações.
